b4d13b5f0811736e97366147491bee845d8234e8
Róbert Kiss <erT78gb@gmail.com>
2017-05-02 01:02:25 +0200

    feat: Add Docker google/cloud-sdk #11

---
+++ src/docker-sdk.js
@@ -0,0 +1,156 @@
+'use strict';
+const EMULATOR_HOST_NAME = 'datastore-emulator';
+const EMULATOR_PORT = '8081';
+
+const path = require('path');
+const Docker = require('dockerode');
+const EmulatorStates = require('./emulator-states');
+const BaseEmulator = require('./base-emulator');
+
+/**
+ * Wrapper for the Docker container engine
+ */
+class DockerSdk extends BaseEmulator {
+  constructor(options) {
+    super(options);
+    this._docker = new Docker();
+    this._container = null;
+  }
+
+  /**
+   * Start the emulator. If need pull the image
+   * @returns {Promise}
+   */
+  start() {
+    const self = this;
+    return new Promise((resolve, reject) => {
+      if (this._state)
+        throw new Error('Datastore (Docker) emulator is already running.');
+
+      super._start(resolve, reject);
+      let cmd = ['gcloud'].concat(self._getCommandParameters());
+
+      const port = EMULATOR_PORT + '/tcp';
+      const bind = [];
+
+      // If data directory set then map the local directory to the container directory
+      if(self._options.dataDir){
+        const abs = path.resolve(self._options.dataDir);
+        bind.push(abs+':/.config/gcloud/emulators/datastore');
+      }
+
+      self._pullImage()
+        .then(() => {
+          return self._docker.createContainer({
+            Hostname: EMULATOR_HOST_NAME,
+            Image: 'google/cloud-sdk',
+            AttachStdin: false,
+            AttachStdout: true,
+            AttachStderr: true,
+            Tty: true,
+            Cmd: cmd,
+            OpenStdin: false,
+            StdinOnce: false,
+            ExposedPorts: {
+              [port]: {}
+            },
+            HostConfig: {
+              PortBindings: {
+                [port]: [
+                  {
+                    HostPort: self._options.port.toString()
+                  }
+                ]
+              },
+              Binds: bind
+            }
+          })
+        })
+        .then((container) => {
+          self._container = container;
+
+          // attach streams to the container
+          container.attach({stream: true, stdout: true, stderr: true}, function (err, stream) {
+            /* istanbul ignore if */
+            if (err)
+              return reject(err);
+
+            // all stream data pass to the processor.
+            stream.on('data', (data) => {
+              const msg = data.toString();
+              self._writeDebug(msg);
+              self._processStd(msg);
+            });
+          });
+
+          return self._container.start();
+        })
+        .catch(reject)
+    })
+  }
+
+  /**
+   * Stop the container
+   * @returns {Promise}
+   */
+  stop() {
+    if (this._state !== EmulatorStates.RUNNING || !this._container)
+      return Promise.resolve();
+
+    return new Promise((resolve) => {
+      super._stop(resolve);
+
+      this._container.stop()
+        .then(() => {
+          this._setState(EmulatorStates.EXIT, 0);
+        });
+    })
+  }
+
+  /**
+   * Pull the docker image
+   * @returns {Object}
+   * @private
+   */
+  _pullImage() {
+    const self = this;
+    return new Promise((resolve, reject)=>{
+      self._docker.pull('google/cloud-sdk', function(err, stream) {
+
+        self._docker.modem.followProgress(stream, onFinished, onProgress);
+
+        function onFinished(err, output) {
+          self._writeDebug(output);
+
+          if(err) return reject(err);
+
+          return resolve();
+        }
+        function onProgress(event) {
+          self._writeDebug(event);
+        }
+      });
+
+    })
+  }
+
+  /**
+   * Set the host and port setting for emulator
+   * @param params
+   * @protected
+   */
+  _setHostPort(params){
+      params.push(`--host-port=${EMULATOR_HOST_NAME}:${EMULATOR_PORT}`);
+  }
+
+  /**
+   * Override the base class abstract method. Not redirect the default working directory when use Docker image
+   * @param params
+   * @protected
+   */
+  _setDatadir(params) {
+  }
+
+}
+
+module.exports = DockerSdk;
---
+++ src/locally-installed-sdk.js
@@ -0,0 +1,130 @@
+'use strict';
+
+const spawn = require("child_process").spawn;
+const kill = require('tree-kill');
+
+const EmulatorStates = require('./emulator-states');
+const BaseEmulator = require('./base-emulator');
+
+/**
+ * Wrapper for the locally installed SDK
+ */
+class LocallyInstalledSdk extends BaseEmulator{
+  constructor(options) {
+    super(options);
+    this._emulator = null;
+  }
+
+  /**
+   * Start the emulator
+   * @returns {Promise}
+   */
+  start() {
+    const self = this;
+
+    return new Promise((resolve, reject) => {
+
+      if (this._state)
+        throw new Error('Datastore emulator is already running.');
+
+      super._start(resolve, reject);
+
+      const params = this._getCommandParameters();
+      self._emulator = spawn('gcloud', params, {shell: true});
+
+      self._registerEmulatorListeners();
+    })
+
+  }
+
+  /**
+   * Stop the emulator
+   * @returns {Promise}
+   */
+  stop() {
+
+    if (this._state !== EmulatorStates.RUNNING)
+      return Promise.resolve();
+
+    return new Promise((resolve) => {
+      super._stop(resolve);
+
+      kill(this._emulator.pid);
+    })
+  }
+
+  /**
+   * Override the base class method. Redirect the emulator working directory
+   * @param params
+   * @protected
+   */
+  _setHostPort(params){
+      params.push(`--host-port=${this._options.host}:${this._options.port}`);
+  }
+
+  /**
+   * Override the base class method. Redirect the emulator working directory
+   * @param params
+   * @protected
+   */
+  _setDatadir(params){
+    if (this._options.dataDir) {
+      this._createDataDirSync();
+      params.push('--data-dir=' + this._options.dataDir);
+    }
+
+  }
+
+  _registerEmulatorListeners() {
+    this._emulator.stdout.on('data', this._emulatorStdOutListener.bind(this));
+
+    this._emulator.stderr.on('data', this._emulatorStdErrListener.bind(this));
+
+    this._emulator.on('close', this._emulatorCloseListener.bind(this));
+
+    this._emulator.on('exit', this._emulatorExitListener.bind(this));
+
+    this._emulator.on('error', this._emulatorErrorListener.bind(this));
+  }
+
+  _removeEmulatorListeners() {
+    this._emulator.stdout.removeListener('data', this._emulatorStdOutListener.bind(this));
+
+    this._emulator.stderr.removeListener('data', this._emulatorStdErrListener.bind(this));
+
+    this._emulator.removeListener('close', this._emulatorCloseListener.bind(this));
+
+    this._emulator.removeListener('exit', this._emulatorExitListener.bind(this));
+
+    this._emulator.removeListener('error', this._emulatorErrorListener.bind(this));
+
+  }
+
+  _emulatorStdOutListener(data) {
+    this._writeDebug(`stdout: ${data}`);
+    this._processStd(data);
+  }
+
+  _emulatorStdErrListener(data) {
+    this._writeDebug(`stderr: ${data}`);
+    this._processStd(data);
+  }
+
+  _emulatorCloseListener(code) {
+    this._writeDebug(`child process close with code ${code}`);
+    this._setState(EmulatorStates.CLOSE, code);
+  }
+
+  _emulatorExitListener(code) {
+    this._writeDebug(`child process exit with code ${code}`);
+    this._setState(EmulatorStates.EXIT, code);
+  }
+
+  _emulatorErrorListener(err) {
+    this._writeDebug(`child process error: ${err}`);
+    this._setState(EmulatorStates.ERROR, err);
+  }
+
+}
+
+module.exports = LocallyInstalledSdk;
