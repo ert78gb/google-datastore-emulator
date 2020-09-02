660e95dac21ec55628e994770a63f8833061be0d
Róbert Kiss <ert78gb@gmail.com>
2020-09-02 23:12:37 +0200

    ci: delete install file

--- scripts/install-cloud-sdk.sh
+++
@@ -1,4 +0,0 @@
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-apt-get update && sudo apt-get install google-cloud-sdk google-cloud-sdk-datastore-emulator
